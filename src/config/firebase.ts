5ec152a37604796e0c5fec7dd921f7b52e351766
Akshay J <jethwaakshay28@gmail.com>
2026-03-25 17:50:24 +0530

    Update application code: new screens & bug fixes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,3 +1,4 @@
+// src/config/firebase.ts
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { 
   getAuth, 
@@ -12,12 +13,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0cRo4vmdwsbNkdiIKwStxGsxJhuhRpYo", 
-  authDomain: "prochemapp-dev.firebaseapp.com",
-  projectId: "prochemapp-dev",
-  storageBucket: "prochemapp-dev.firebasestorage.app",
-  messagingSenderId: "193158013078",
-  appId: "1:193158013078:web:af1b9968ab61a561a662b7"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY, 
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 let app: FirebaseApp;
